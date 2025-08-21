99d7cffe4efffc3420caecd0c3dd480e793cf201
Vennus <vennussalguero@gmail.com>
2025-08-21 16:53:02 -0600

    Add files via upload

---
+++ EmpleadosPrueba/Consulta.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace EmpleadosPrueba
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgb_1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgb_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar Datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgb_1
+            // 
+            this.dgb_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgb_1.Location = new System.Drawing.Point(21, 79);
+            this.dgb_1.Name = "dgb_1";
+            this.dgb_1.RowHeadersWidth = 51;
+            this.dgb_1.RowTemplate.Height = 24;
+            this.dgb_1.Size = new System.Drawing.Size(488, 346);
+            this.dgb_1.TabIndex = 1;
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 450);
+            this.Controls.Add(this.dgb_1);
+            this.Controls.Add(this.button1);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            ((System.ComponentModel.ISupportInitialize)(this.dgb_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgb_1;
+    }
+}
---
+++ EmpleadosPrueba/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace EmpleadosPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Txt_depto = new System.Windows.Forms.TextBox();
+            this.Txt_puesto = new System.Windows.Forms.TextBox();
+            this.Txt_nombre = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_consulta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 42);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Recetas";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(23, 86);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 17);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Nombre";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 124);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 17);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Puesto";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(23, 159);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(98, 17);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Departamento";
+            // 
+            // Txt_depto
+            // 
+            this.Txt_depto.Location = new System.Drawing.Point(139, 157);
+            this.Txt_depto.Name = "Txt_depto";
+            this.Txt_depto.Size = new System.Drawing.Size(286, 22);
+            this.Txt_depto.TabIndex = 4;
+            // 
+            // Txt_puesto
+            // 
+            this.Txt_puesto.Location = new System.Drawing.Point(139, 121);
+            this.Txt_puesto.Name = "Txt_puesto";
+            this.Txt_puesto.Size = new System.Drawing.Size(286, 22);
+            this.Txt_puesto.TabIndex = 5;
+            // 
+            // Txt_nombre
+            // 
+            this.Txt_nombre.Location = new System.Drawing.Point(139, 81);
+            this.Txt_nombre.Name = "Txt_nombre";
+            this.Txt_nombre.Size = new System.Drawing.Size(286, 22);
+            this.Txt_nombre.TabIndex = 6;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(22, 208);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(112, 48);
+            this.btn_agregar.TabIndex = 7;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(170, 208);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(112, 48);
+            this.btn_eliminar.TabIndex = 8;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // btn_consulta
+            // 
+            this.btn_consulta.Location = new System.Drawing.Point(313, 208);
+            this.btn_consulta.Name = "btn_consulta";
+            this.btn_consulta.Size = new System.Drawing.Size(112, 48);
+            this.btn_consulta.TabIndex = 9;
+            this.btn_consulta.Text = "Consulta";
+            this.btn_consulta.UseVisualStyleBackColor = true;
+            this.btn_consulta.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(486, 328);
+            this.Controls.Add(this.btn_consulta);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.Txt_nombre);
+            this.Controls.Add(this.Txt_puesto);
+            this.Controls.Add(this.Txt_depto);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox Txt_depto;
+        private System.Windows.Forms.TextBox Txt_puesto;
+        private System.Windows.Forms.TextBox Txt_nombre;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_consulta;
+    }
+}
+
